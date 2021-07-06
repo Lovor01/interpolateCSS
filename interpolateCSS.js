ac352afb1f6ea037ea4af651bbc093355d4a655f
Lovor01 <33687174+Lovor01@users.noreply.github.com>
2021-07-06 23:37:33 +0200

    V1.2.0

--- interpolateCSS.js
+++ interpolateCSS.js
@@ -5,7 +5,7 @@
  * All Rights Reserved.
  * Use, reproduction, distribution, and modification of this code is subject to the terms and
  * conditions of the MIT license, available at http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * intended to solve dynamic change of CSS properties, interpolation and relational changes,
  * when one element property changes, change the other accordingly
  *
@@ -13,11 +13,11 @@
  * fires interpolateCSSDone event when all interpolations are finished
  * raises warning if some elements in config objects are not valid or do not exist
  *
- * version 1.1.0
+ * version 1.2.0
  *
  *
  * accepts config object with following properties:
- * element : selector string or DOM element
+ * element : selector string or DOM element (multiple elements allowed)
  * property : property being changed, could be given as css-style (line-height) or Javascript style (lineHeight)
  * unit : unit for the property being changed
  * xBreakpoints: array of breakpoints for x between which to interpolate, currently must be sorted from smallest to biggest
@@ -27,9 +27,14 @@
  * extrapolateMin : extrapolate lower than smallest breakpoint. If this is not set or false, property value is fixed on lowest y value for lower values of x
  * extrapolateMax : extrapolate higher than highest breakpoint. If this is not set or false, property value is fixed on greatest y value for greater values of x
  * minYValue, maxYValue not yet applied
+ *
+ * interpolateCSSDone event is dispatched when interpolation is finished
+ *
+ * improved from v1.1.0 - selection on multiple elements, multiple elements can depend on one xDefinition, interpolateCSSDone fires only once upon finish, not on every element
  */
 'use strict';
 function interpolateCSS(config) {
+
 
 	const evDone = new Event('interpolateCSSDone');
 
@@ -41,15 +46,36 @@ function interpolateCSS(config) {
 			console.warn('Provided element number ' + index + ' is not DOM element, interpolation will not work on element! \nElement: ' + curel.element )
 		}
 
-		function correctCSSpropToCamelCase() {
+		/* function correctCSSpropToCamelCase() {
 			let dashPos = curel.property.indexOf('-');
 			if (dashPos !== -1)
 				curel.property = curel.property.substring(0, dashPos-1) + curel.property.substring(dashPos+1, dashPos+2).toUpperCase() + curel.property.substring(dashPos+2);
+		} */
+
+		// if yValue is object it has to have at least multiply property
+		function validConfigYValues() {
+			function isValidSingle(el) {
+				return (typeof el === 'number') || (typeof el === 'object' && el.hasOwnProperty('multiply'));
+			}
+			if (curel.singleY)
+				return isValidSingle(curel.yValues);
+			else
+			{
+				let valid = true;
+				curel.yValues.forEach(function(el) { valid = valid && isValidSingle(el) });
+				return valid;
+			}
 		}
 
 		// mark if yValues is array - do not check typeof many times, query variable
-		curel.singleY = typeof curel.yValues === 'number';
 
+		curel.singleY = ! Array.isArray(curel.yValues);
+		if (! validConfigYValues()) {
+			console.error('Given yValues is misconfigured; not number nor Array or property multiply does not exists! Stopping.');
+			return;
+		}
+
+		// if element is string, find DOM element(s)
 		if (typeof curel.element === 'string') {
 			let tempEl = document.querySelectorAll(curel.element);
 			tempEl.forEach(function(el){
@@ -84,17 +110,12 @@ function interpolateCSS(config) {
 				curel.element = undefined;
 			}
 		}
+
+		// if there is only single y, spread to all breakpoints
 		if (curel.singleY && curel.xBreakpoints)
 			curel.yValues = curel.xBreakpoints.map(function() {return curel.yValues} )
 
-		// resize observer
-		const resObserver = new ResizeObserver(doInterps);
-		if (curel.element instanceof NodeList)
-			curel.element.forEach(function(el) {
-				resObserver.observe(el);
-			})
-		else
-			resObserver.observe(curel.element)
+
 	});
 
 	//  remove empty elements
@@ -112,20 +133,17 @@ function interpolateCSS(config) {
 		return;
 
 	// set interpolation on resize and run initially
-	window.addEventListener('resize', doInterps);
-	// if doMutations is requested, set observing DOM mutations
-	if (config.doMutations) {
-		const mutObserver = new MutationObserver(doInterps);
-		mutObserver.observe(document.querySelector('body'));
-	}
+	window.addEventListener('resize', doInterpolations);
 
 
-	doInterps();
+	interpolateCSS.doInterpolations = doInterpolations;
+
+	doInterpolations();
 
 	/**
 	 * Callback that does calculations and interpolation
 	 */
-	function doInterps() {
+	function doInterpolations() {
 		// set requestAnimationFrame to throttle event handler
 		window.requestAnimationFrame(function() {
 
@@ -139,8 +157,8 @@ function interpolateCSS(config) {
 
 			/**
 			 * subfunction to interpolate and write css style
-			 * @param  {DOM element} el
-			 * @param  {integer} index
+			 * @param  {DOM element} el element, with one or more values for y between to interpolate
+			 * @param  {integer} index index of breakpoints between which it is interpolated
 			 * @param  {float} x
 			 */
 			function linearInterpolation(el, index, x) {
@@ -156,7 +174,7 @@ function interpolateCSS(config) {
 
 			function separateValueAndUnit(inValue) {
 				let numeric = parseFloat(inValue);
-				return [numeric, inValue.substring(numeric.toString().length)]
+				return { value: numeric, unit: inValue.substring(numeric.toString().length)}
 			}
 
 			// outer scope variables to be accessible everywhere
@@ -166,28 +184,45 @@ function interpolateCSS(config) {
 			// main loop iterating all settings
 			config.forEach(function(configEl) {
 
-				let x, rArray;
-				// window width is default x
-				x = window.innerWidth;
+				// x is compared with breakpoints
+				// rArray is object of value and unit - a structure. Unit is string
+				let x = null, rArray;
+				// prepare yValues function
+
 				// check if xDefinition exists and prepare variables accordingly
+				// this means that y is calculated from x, otherwise it is simple value
 				if (configEl.hasOwnProperty('xDefinition')) {
+					let getCalculatedY;
+					if (typeof configEl.yValues === 'object' )
+						// function to facilitate calculation of y if it is object of multiply, add
+						getCalculatedY = function(x, y) { return x * y.multiply + y.add; };
+					else
+						getCalculatedY = function(x, y) { return x * y }
 					// rArray is array of value [0] and unit [1]
 					rArray = separateValueAndUnit(
 						window.getComputedStyle(configEl.xDefinition.element)[configEl.xDefinition.property]
 					);
-					// calculate y
+					// calculate y, yValues_calc spreads yValues on all Breakpoints
 					if (configEl.singleY)
-						configEl.yValues_calc = configEl.yValues * rArray[0]
+						configEl.yValues_calc = getCalculatedY(rArray.value, configEl.yValues);
 					else
-						configEl.yValues_calc = configEl.yValues.map(function(el){
-							return el * rArray[0];
+						configEl.yValues_calc =  configEl.yValues.map(function(el){
+							return getCalculatedY(rArray.value, el);
 						});
+					// unit is not obligatory, if not defined it takes unit from xDefinition object
 					if (! configEl.hasOwnProperty('unit'))
-						configEl.unit = rArray[1];
+						configEl.unit = rArray.unit;
+					if (! configEl.xDefinition.breakPointsFromElement)
+						x = window.innerWidth;
 				}
-				else
+				else {
+					// no xDefinition, things are simpler
 					configEl.yValues_calc = configEl.yValues;
+					rArray = {value: window.innerWidth, unit: 'px'}
+				};
 
+				if (! x)
+					x = rArray.value;
 
 
 
@@ -206,7 +241,7 @@ function interpolateCSS(config) {
 						if ((x >= configEl.xBreakpoints[index] && x <= configEl.xBreakpoints[index + 1]) ||
 						(configEl.extrapolateMin && index == 0 && x < configEl.xBreakpoints[index]))
 						{
-							linearInterpolation(configEl, index, rArray !== undefined ? rArray[0] : x);
+							linearInterpolation(configEl, index, rArray.value);
 							break;
 						}
 					}
@@ -218,9 +253,9 @@ function interpolateCSS(config) {
 						setCSSProp(configEl.element, configEl.property,
 							function(element, property) { element.style[property] = '' })
 				}
-
-				window.dispatchEvent(evDone);
 			});
+			window.dispatchEvent(evDone);
+
 		});
 	}
 }
