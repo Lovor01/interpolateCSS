f8b7db85247c0cb1075c6403023feaaf07c3a635
Lovor01 <33687174+Lovor01@users.noreply.github.com>
2021-07-07 00:54:48 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ interpolateCSS([
 		property: "font-size",
 		unit: 'px',
 		yValues: [18, 14, 26],
-    xBreakpoints: [460, 1024, 1680],
+    	xBreakpoints: [460, 1024, 1680],
 	},
 	{
 		element: '.point75',
@@ -13,6 +13,17 @@ interpolateCSS([
 		yValues: 0.75,
 		xDefinition: {
 			element: 'main h2',
-			property: 'font-size' },
+			property: 'font-size' 
+		}
+	},
+	{
+		element: '.box-fontx2',
+		property: "height",
+		unit: 'px',
+		yValues: 0.75,
+		xDefinition: {
+			element: 'main h2',
+			property: 'font-size' 
+		}
 	}
 ])
