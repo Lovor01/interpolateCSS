b25e6eb05de9000ca062b0b53099c6a0bed8ccd4
Lovor01 <33687174+Lovor01@users.noreply.github.com>
2021-07-07 00:45:00 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+interpolateCSS([
+	{
+		element: 'main h2',
+		property: "font-size",
+		unit: 'px',
+		yValues: [18, 14, 26],
+    xBreakpoints: [460, 1024, 1680],
+	},
+	{
+		element: '.point75',
+		property: "font-size",
+		unit: 'px',
+		yValues: 0.75,
+		xDefinition: {
+			element: 'main h2',
+			property: 'font-size' },
+	}
+])
